57e3fceaf3fa6dc16c0c852b2bae69e83ecac56e
Lukas Basse <lukasbasse22@gmail.com>
2022-09-26 13:41:52 -0400

    Removed redundant code

--- src/main.tsx
+++ src/main.tsx
@@ -13,25 +13,6 @@ import {
   BrowserRouter,
 } from "react-router-dom";
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Home />,
-  },
-  {
-    path: "/home",
-    element: <Home />,
-  },
-  {
-    path: "/about",
-    element: <About />,
-  },
-  {
-    path: "/contact",
-    element: <Contact />,
-  },
-]);
-
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <BrowserRouter>
