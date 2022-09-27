60917112c4bcd9fc662ba9441179fe42afed3b23
boor3d <jest3r.boor3d@gmail.com>
2022-09-27 11:24:59 -0500

    import Contact.css Fix

--- src/routes/Contact.tsx
+++ src/routes/Contact.tsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
 import reactLogo from '../assets/react.svg'
-import './Home.css'
+import './Contact.css'
 
 function App() {
   const [count, setCount] = useState(0);
